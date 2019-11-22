0d289140e73ea624b3c5ed02a790596b3a078d76
rafulll <raful@aluno.fapce.edu.br>
2019-11-22 02:21:37 -0300

    correct index.php

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -22,7 +22,21 @@ final class Routes {
 
     private function initRoutesV1() {
         $app = $this->app;
+        $container = $app->getContainer();
 
+        // Register component on container
+        $container['view'] = function ($container) {
+            $view = new \Slim\Views\Twig('.\views',[
+                'cache' => false
+            ]);
+        
+            // Instantiate and add Slim specific extension
+            $router = $container->get('router');
+            $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+            $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+        
+            return $view;
+        };
         $app->group("/v1", function() use ($app) {
 
             /* Métodos GET */
--- index.php
+++ index.php
@@ -4,13 +4,11 @@ require_once "env.php";
 require_once "app/config/Constants.php";
 
 use Slim\App;
-use Psr\Http\Message\MessageInterface as RQ;
 use Config\Config;
 use Config\Routes;
-use Psr\Http\Message\MessageInterface;
+
 
 $app = new App(Config::getSlimConfig());
-
 new Routes($app);
 
 $app->run();
